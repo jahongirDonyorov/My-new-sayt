87050a14d0ff8eadbe185c8e787f62e168935e01
Jahon <donyorovjahon@gmail.com>
2023-04-02 19:48:01 +0100

    JavaScript

--- js/main.js
+++ js/main.js
@@ -7,126 +7,124 @@ window.addEventListener("DOMContentLoaded", () => {
   //   }, 1500)
   // }, 2000)
 
- 
   // novbar uzgaruvchilar
-  let nextClick = document.querySelector('.next'),
-    bars = document.querySelector('.click__left'),
-    bar = document.querySelector('.click__left--end-list'),
-    exitClick = document.querySelector('.icons--x'),
-    extli = document.querySelector('.extLi'),
-    clear = document.querySelector('.icons--xxx'),
-    inputClear = document.querySelector('.input__clear'),
-    // sorch 
-    sorch = document.querySelector('.sorch--x'),
-    poisk = document.querySelector('.poisk'),
-    poiskExt = document.querySelector('.icons--xx'),
-    globalSorch = document.querySelector('.sorch');
+  let nextClick = document.querySelector(".next"),
+    bars = document.querySelector(".click__left"),
+    bar = document.querySelector(".click__left--end-list"),
+    exitClick = document.querySelector(".icons--x"),
+    extli = document.querySelector(".extLi");
+  //===== Eski  sorch codlari
+  // clear = document.querySelector('.icons--xxx'),
+  // inputClear = document.querySelector('.input__clear'),
+  // sorch = document.querySelector('.sorch--x'),
+  // poisk = document.querySelector('.poisk'),
+  // poiskExt = document.querySelector('.icons--xx'),
+  // globalSorch = document.querySelector('.sorch');
 
-
-  nextClick.addEventListener('click', function () {
-    bars.style.cssText = 'display: flex; animation-name: anime; ';
-    bar.style.display = 'block';
+  nextClick.addEventListener("click", function () {
+    bars.style.cssText = "display: flex; animation-name: anime; ";
+    bar.style.display = "block";
     // console.log(1)
-  })
-  exitClick.addEventListener('click', function () {
-    bars.style.cssText = 'display: none; ';
-  })
-  extli.addEventListener('click', function (event) {
-    bars.style.cssText = 'display: none; ';
-  })
+  });
+  exitClick.addEventListener("click", function () {
+    bars.style.cssText = "display: none; ";
+  });
+  extli.addEventListener("click", function (event) {
+    bars.style.cssText = "display: none; ";
+  });
 
+  //===== Eski sorch codlari
+  // poisk.addEventListener('click', () => {
+  //   globalSorch.style.top = '0'
+  // })
+  // poiskExt.addEventListener('click', () => {
+  //   globalSorch.style.top = '-525px'
+  // })
+  // //sorch > inputga
+  // clear.addEventListener('click', () => {
+  //   inputClear.value = ''
+  //   clear.style.display = 'none'
+  //   sorch.style.display = 'block'
+  // })
 
+  // inputClear.addEventListener('click', () => {
+  //   sorch.style.display = 'none'
+  //   clear.style.display = 'block'
+  // })
 
-
-  //sorch next ext
-  poisk.addEventListener('click', () => {
-    globalSorch.style.top = '0'
-  })
-  poiskExt.addEventListener('click', () => {
-    globalSorch.style.top = '-525px'
-  })
-  //sorch > inputga
-  clear.addEventListener('click', () => {
-    inputClear.value = ''
-    clear.style.display = 'none'
-    sorch.style.display = 'block'
-  })
-
-  inputClear.addEventListener('click', () => {
-    sorch.style.display = 'none'
-    clear.style.display = 'block'
-  })
   // shoop minu
-  let shopping = document.querySelector('.shopping'),
-    shoppingX = document.querySelector('.shopping-x'),
-    beskit = document.querySelector('.beskit'),
-    beskitExt = document.querySelector('.beskit__nav--icon');
+  let shopping = document.querySelector(".shopping"),
+    shoppingX = document.querySelector(".shopping-x"),
+    beskit = document.querySelector(".beskit"),
+    beskitExt = document.querySelector(".beskit__nav--icon");
 
-  shoppingX.addEventListener('click', () => {
-    beskit.style.cssText = 'transform: translateX(0);'
-  })
-  shopping.addEventListener('click', () => {
-    beskit.style.cssText = 'transform: translateX(0);'
-  })
-  beskitExt.addEventListener('click', () => {
-    beskit.style.cssText = 'transform: translateX(500px);'
+  shoppingX.addEventListener("click", () => {
+    beskit.style.cssText = "transform: translateX(0);";
+  });
+  shopping.addEventListener("click", () => {
+    beskit.style.cssText = "transform: translateX(0);";
+  });
+  beskitExt.addEventListener("click", () => {
+    beskit.style.cssText = "transform: translateX(500px);";
+  });
+  // CALL CENTER
+  const CallNexit = document.querySelector(".call"),
+    CallExit = document.querySelector(".icons--xy"),
+    CallBox = document.querySelector(".main__call");
 
-  })
-    // CALL CENTER
-    const CallNexit = document.querySelector('.call'),
-    CallExit = document.querySelector('.icons--xy'),
-    CallBox = document.querySelector('.main__call');
-
-    CallNexit.addEventListener('click', () => {
+  CallNexit.addEventListener("click", () => {
     console.log(1);
-    CallBox.style.display = 'flex'
-    CallNexit.style.display = 'none'
-    })
+    CallBox.style.display = "flex";
+    CallNexit.style.display = "none";
+  });
 
-    CallExit.addEventListener('click', () => {
-      console.log(1);
-      CallBox.style.display = 'none'
-      CallNexit.style.display = 'flex'
-
-      });
+  CallExit.addEventListener("click", () => {
+    console.log(1);
+    CallBox.style.display = "none";
+    CallNexit.style.display = "flex";
+  });
 
   //  ====================== slider corusel
 
-  const slider = document.querySelector('.slider__corusel--sled'),
-    leftArrow = document.querySelector('.left'),
-    rightArrow = document.querySelector('.right'),
-    indicatorParents = document.querySelector('.slider__controls ul')
+  const slider = document.querySelector(".slider__corusel--sled"),
+    leftArrow = document.querySelector(".left"),
+    rightArrow = document.querySelector(".right"),
+    indicatorParents = document.querySelector(".slider__controls ul");
 
   let sectionIndex = 0;
 
-  leftArrow.addEventListener('click', () => {
-    sectionIndex = (sectionIndex > 0) ? sectionIndex - 1 : 3;
-    document.querySelector('.slider__controls .selectid').classList.remove('selectid')
-    indicatorParents.children[sectionIndex].classList.add('selectid')
-    slider.style.transform = 'translate(' + (sectionIndex) * -25 + '%)'
-  })
+  leftArrow.addEventListener("click", () => {
+    sectionIndex = sectionIndex > 0 ? sectionIndex - 1 : 3;
+    document
+      .querySelector(".slider__controls .selectid")
+      .classList.remove("selectid");
+    indicatorParents.children[sectionIndex].classList.add("selectid");
+    slider.style.transform = "translate(" + sectionIndex * -25 + "%)";
+  });
 
+  rightArrow.addEventListener("click", () => {
+    sectionIndex = sectionIndex < 3 ? sectionIndex + 1 : 0;
+    document
+      .querySelector(".slider__controls .selectid")
+      .classList.remove("selectid");
+    indicatorParents.children[sectionIndex].classList.add("selectid");
+    slider.style.transform = "translate(" + sectionIndex * -25 + "%)";
+  });
 
-  rightArrow.addEventListener('click', () => {
-    sectionIndex = (sectionIndex < 3) ? sectionIndex + 1 : 0;
-    document.querySelector('.slider__controls .selectid').classList.remove('selectid')
-    indicatorParents.children[sectionIndex].classList.add('selectid')
-    slider.style.transform = 'translate(' + (sectionIndex) * -25 + '%)'
-  })
+  document
+    .querySelectorAll(".slider__controls li")
+    .forEach(function (indicator, ind) {
+      indicator.addEventListener("click", () => {
+        sectionIndex = ind;
+        document
+          .querySelector(".slider__controls .selectid")
+          .classList.remove("selectid");
 
-  document.querySelectorAll('.slider__controls li').forEach(function (indicator, ind) {
-    indicator.addEventListener('click', () => {
-      sectionIndex = ind;
-      document.querySelector('.slider__controls .selectid').classList.remove('selectid')
-
-      indicator.classList.add('selectid');
-      slider.style.transform = 'translate(' + (sectionIndex) * -25 + '%)'
-
-    })
-  })
-
-
-})
+        indicator.classList.add("selectid");
+        slider.style.transform = "translate(" + sectionIndex * -25 + "%)";
+      });
+    });
+});
 
 //================================ scroll animation
 window.addEventListener("scroll", reveal);
@@ -148,80 +146,75 @@ function reveal() {
 }
 
 //================================ phone bars
-let phoneBars = document.querySelector('.phone__bar'),
-  phone = document.querySelector('.phone'),
-  phoneX = document.querySelector('.icons--xxxxx');
+let phoneBars = document.querySelector(".phone__bar"),
+  phone = document.querySelector(".phone"),
+  phoneX = document.querySelector(".icons--xxxxx");
 
-phoneBars.addEventListener('click', () => {
-  phone.style.display = 'flex';
-})
-phoneX.addEventListener('click', () => {
-  phone.style.display = 'none'
-})
-
+phoneBars.addEventListener("click", () => {
+  phone.style.display = "flex";
+});
+phoneX.addEventListener("click", () => {
+  phone.style.display = "none";
+});
 
 //=========================------------ info index uchun
 //====== malumotlar (info)
 
-
-const tabs = document.querySelectorAll('.tabs_a'),
-  tabContent = document.querySelectorAll('.box-right-foot'),
+const tabs = document.querySelectorAll(".tabs_a"),
+  tabContent = document.querySelectorAll(".box-right-foot"),
   headerParents = document.querySelector(".box-right-tabs-list");
 
 function hideTabContent() {
-  tabContent.forEach((item) => {
-    item.style.display = 'none';
-  });
+  // tabContent.forEach((item) => {
+  //   item.style.display = 'none';
+  // });
   tabs.forEach((item) => {
-    item.classList.remove('tabs__active');
+    item.classList.remove("tabs__active");
   });
 }
 
 function showTabContent(i = 0) {
-  tabContent[i].style.display = 'block';
-  tabs[i].classList.add('tabs__active');
+  // tabContent[i].style.display = 'block';
+  tabs[i].classList.add("tabs__active");
 }
 
 hideTabContent();
 showTabContent();
 
-headerParents.addEventListener('click', (event) => {
+headerParents.addEventListener("click", (event) => {
   const target = event.target;
-  if (target && target.classList.contains('tabs_a')) {
+  if (target && target.classList.contains("tabs_a")) {
     tabs.forEach((item, i) => {
       if (target == item) {
         hideTabContent();
         showTabContent(i);
       }
-    })
+    });
   }
 });
 
-const minus = document.querySelector('.amount__actions--icon-minus'),
-  plus = document.querySelector('.amount__actions--icon-plus'),
-  input = document.querySelector('.num'),
-  sum = document.querySelector('.sum'),
-  defaultSum = document.querySelector('.defaultSum')
+const minus = document.querySelector(".amount__actions--icon-minus"),
+  plus = document.querySelector(".amount__actions--icon-plus"),
+  input = document.querySelector(".num"),
+  sum = document.querySelector(".sum"),
+  defaultSum = document.querySelector(".defaultSum");
 
 let a = 1;
 let b = [];
-b[1] = defaultSum.innerText
+b[1] = defaultSum.innerText;
 
-plus.addEventListener('click', () => {
+plus.addEventListener("click", () => {
   a++;
-  a = (a < 10) ? '0' + a : a;
+  a = a < 10 ? "0" + a : a;
   input.innerText = a;
   sum.innerText = +sum.innerText + +b[1];
 });
-console.log(b)
-minus.addEventListener('click', () => {
+console.log(b);
+minus.addEventListener("click", () => {
   if (a > 1) {
     a--;
-    a = (a < 10) ? '0' + a : a;
+    a = a < 10 ? "0" + a : a;
     input.innerText = a;
-  sum.innerText = +sum.innerText - +b[1];
-
+    sum.innerText = +sum.innerText - +b[1];
   }
 });
-
-   
